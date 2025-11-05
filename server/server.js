d888f87acb426bc026a5a3d337c7bd787576a248
Poorvik13 <sapoorvik444@gmail.com>
2025-11-05 19:09:17 +0530

    Update code changes

--- server/server.js
+++ server/server.js
@@ -44,9 +44,13 @@ app.use("/api/messages", messageRouter);
 
 //Connect to Mangodb
 await connectDB();
-
-
-const PORT = process.env.PORT || 5000;
+if(process.env.NODE_ENV !== "production"){
+    const PORT = process.env.PORT || 5000;
 server.listen(PORT, ()=> console.log("server is running on PORT: " + PORT)
 
 );
+}
+// export server for vercel
+
+export  default server;
+
